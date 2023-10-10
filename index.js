3bdfe56d6dab480cad7cc98d938d04ccbeaa6514
DEXTREME <mdkurniawan193@gmail.com>
2023-10-10 14:50:19 +0700

    revisi

--- index.js
+++ index.js
@@ -47,59 +47,59 @@ router.post('/registrasi', async (req, res) => {
     }
 });
 
-router.post('/login', function (req, res, next) {
-    if (req.body.username == "" || req.body.password == "") {
-        res.status(400).json({
-            message: "EMPTY FIELD"
-        });
-        return;
-    }
+// router.post('/login', function (req, res, next) {
+//     if (req.body.username == "" || req.body.password == "") {
+//         res.status(400).json({
+//             message: "EMPTY FIELD"
+//         });
+//         return;
+//     }
 
-    db.user.findOne({
-        where: {
-            username: req.body.username,
-            password: req.body.password
-        }
-    })
-    .then(function(data) {
-        if (data) {
-            // Pengguna ditemukan, kirim respons login berhasil
-            res.status(200).json({
-                message: "success login",
-                data: data
-            });
-        } else {
-            // Pengguna tidak ditemukan, kirim respons login gagal
-            res.status(401).json({
-                message: "Login failed: username or password is incorrect"
-            });
-        }
-    })
-    .catch(function(err) {
-        console.log(err);
-        res.status(500).json({
-            message: err
-       });
-});
-});
+//     db.user.findOne({
+//         where: {
+//             username: req.body.username,
+//             password: req.body.password
+//         }
+//     })
+//     .then(function(data) {
+//         if (data) {
+//             // Pengguna ditemukan, kirim respons login berhasil
+//             res.status(200).json({
+//                 message: "success login",
+//                 data: data
+//             });
+//         } else {
+//             // Pengguna tidak ditemukan, kirim respons login gagal
+//             res.status(401).json({
+//                 message: "Login failed: username or password is incorrect"
+//             });
+//         }
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//         res.status(500).json({
+//             message: err
+//        });
+// });
+// });
  // Membuat route POST untuk login
-    // router.post('/login', async (req, res, next) => {
-    //   try {
-    //     const { username, password } = req.body;
-    //     // Find the user by email
-    //     const user = await User.findOne({ where: { username:username } });
-    //     if (!user) {
-    //       return res.status(404).json({ success: false, message: 'User not found' });
-    //     }
-    //     if (password !== user.password) {
-    //       return res.status(401).json({ success: false, message: 'Invalid password' });
-    //     }
-    //     // Authentication successful
-    //     res.json({ success: true, message: 'Login successful', user });
-    //   } catch (error) {
-    //     res.status(500).json({ success: false, error: error.message });
-    //   }
-    // })
+    router.post('/login', async (req, res, next) => {
+      try {
+        const { username, password } = req.body;
+        // Find the user by email
+        const user = await User.findOne({ where: { username:username } });
+        if (!user) {
+          return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        if (password !== user.password) {
+          return res.status(401).json({ success: false, message: 'Invalid password' });
+        }
+        // Authentication successful
+        res.json({ success: true, message: 'Login successful', user });
+      } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+      }
+    })
 
       router.post('/booking', async (req, res) => {
         const { nama, noHP, email, service } = req.body;
