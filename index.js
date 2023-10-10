ca6170a9ced912a0a82a18e30e30dbdfc7944f9d
DEXTREME <mdkurniawan193@gmail.com>
2023-10-10 14:38:44 +0700

    revisimulu

--- index.js
+++ index.js
@@ -48,33 +48,23 @@ router.post('/registrasi', async (req, res) => {
 });
 
  // Membuat route POST untuk login
-      router.post('/login', function (req, res, next) {
-      if (req.body.username == "" || req.body.password == ""){
-      res.status(400).json({
-          message: "EMPTY FIELD"
-      })
-      return
-    }
-   
-    db.user.findOne({
-      where: {
-          username: req.body.username,
-          password: req.body.password
+    router.post('/login', async (req, res, next) => {
+      try {
+        const { username, password } = req.body;
+        // Find the user by email
+        const user = await User.findOne({ where: { username:username } });
+        if (!user) {
+          return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        if (password !== user.password) {
+          return res.status(401).json({ success: false, message: 'Invalid password' });
+        }
+        // Authentication successful
+        res.json({ success: true, message: 'Login successful', user });
+      } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
       }
-      })
-    then(function(data){
-      res.status(200).json({
-          message: "success login",
-          data: data
-      })
     })
-    .catch(function(err){
-      console.log(err)
-      res.status(500).json({
-          message: err
-      })
-     })
-      });
 
       router.post('/booking', async (req, res) => {
         const { nama, noHP, email, service } = req.body;
